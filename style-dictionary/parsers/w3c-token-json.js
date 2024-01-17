bf159709827dc83fab667360ffdf85687ae7d8d0
Luke Fantom <luke.fantom@jet2.com>
2024-01-17 11:26:26 +0000

    updated w3c parser

--- style-dictionary/parsers/w3c-token-json.js
+++ style-dictionary/parsers/w3c-token-json.js
@@ -2,7 +2,10 @@ module.exports = {
   pattern: /\.json$|\.tokens\.json$|\.tokens$/,
   parse: ({ contents }) => {
     // replace $value/$type with value/type so that style dictionary recognizes it
-    const preparedContent = (contents || "{}").replace(/"\$?value"\s*:/g, '"value":').replace(/"\$?type"\s*:/g, '"type":');
+    const preparedContent = (contents || "{}")
+      .replace(/"\$?value"\s*:/g, '"value":')
+      .replace(/"\$?type"\s*:/g, '"type":')
+      .replace(/"\$?description"\s*:/g, '"description":');
     return JSON.parse(preparedContent);
   },
 };
